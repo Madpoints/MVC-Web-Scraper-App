b3cea60b314f5b04e25d34a3426d6694bbde90bd
John Furtado <jf8846@gmail.com>
2018-11-01 15:03:06 -0400

    registration page get mapping

--- src/main/java/com/madpoints/webscraper/controller/RegisterController.java
+++ src/main/java/com/madpoints/webscraper/controller/RegisterController.java
@@ -1,8 +1,15 @@
 package com.madpoints.webscraper.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class RegisterController {
+	
+	@GetMapping("/register")
+	public String showRegistration() {
+		
+		return "register";
+	}
 
 }
