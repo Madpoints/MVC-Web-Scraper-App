a50d191010dd37abe38b44995f76c1f9ff60d6fd
John Furtado <jf8846@gmail.com>
2018-11-05 17:10:45 -0500

    added request mapping, changed get mapping, removed post mapping

--- src/main/java/com/madpoints/webscraper/controller/RegisterController.java
+++ src/main/java/com/madpoints/webscraper/controller/RegisterController.java
@@ -2,21 +2,16 @@ package com.madpoints.webscraper.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/register")
 public class RegisterController {
 	
-	@GetMapping("/register")
+	@GetMapping("/new")
 	public String showRegistration() {
 		
 		return "register";
 	}
 
-	@PostMapping("/register")
-	public String register() {
-		
-		return "submit";
-	}
-	
 }
