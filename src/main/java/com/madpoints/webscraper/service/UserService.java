4d286e36e0e2d82640e8a5c030b0a8f604ea00f3
John Furtado <jf8846@gmail.com>
2018-11-11 17:37:34 -0500

    repository annotation

--- src/main/java/com/madpoints/webscraper/service/UserService.java
+++ src/main/java/com/madpoints/webscraper/service/UserService.java
@@ -1,7 +1,10 @@
 package com.madpoints.webscraper.service;
 
+import org.springframework.stereotype.Repository;
+
 import com.madpoints.webscraper.entity.User;
 
+@Repository
 public class UserService {
 
 	public void registerUser(User newUser) {
