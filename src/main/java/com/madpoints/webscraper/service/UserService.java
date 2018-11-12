3d366e4813f633902848ef555849f3bf64cdf1a8
John Furtado <jf8846@gmail.com>
2018-11-12 09:59:08 -0500

    autowire hibernate session factory

--- src/main/java/com/madpoints/webscraper/service/UserService.java
+++ src/main/java/com/madpoints/webscraper/service/UserService.java
@@ -1,13 +1,20 @@
 package com.madpoints.webscraper.service;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.madpoints.webscraper.entity.User;
 
 @Repository
 public class UserService {
+	
+	// inject session factory
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	public void registerUser(User newUser) {
+		
 		
 	}
 }
