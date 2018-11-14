2e66d9ad093006ee1e63f6c0bd91c0d20f11ae5f
John Furtado <jf8846@gmail.com>
2018-11-14 14:56:00 -0500

    user service interface

--- src/main/java/com/madpoints/webscraper/service/UserService.java
+++ src/main/java/com/madpoints/webscraper/service/UserService.java
@@ -1,31 +1,13 @@
 package com.madpoints.webscraper.service;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.madpoints.webscraper.entity.User;
 
-@Repository
-public class UserService {
-	
-	// inject session factory
-	@Autowired
-	private SessionFactory sessionFactory;
+public interface UserService {
 
-	public void registerUser(User newUser) {
-		
-	}
+	public User getUser(int userId);
 	
-	@Transactional
-	public User getUser() {
-		
-		Session currentSession = sessionFactory.getCurrentSession();
-		
-		User theUser = currentSession.get(User.class, 1);
-		
-		return theUser;
-	}
+	public void registerUser(User theUser);
+	
+	public void deleteUser(int userId);
+	
 }
