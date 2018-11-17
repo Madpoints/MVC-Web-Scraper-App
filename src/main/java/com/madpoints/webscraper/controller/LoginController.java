933b7ac0d180654e1d3bbb00c99d04723ad75c83
John Furtado <jf8846@gmail.com>
2018-11-17 16:54:12 -0500

    login user

--- src/main/java/com/madpoints/webscraper/controller/LoginController.java
+++ src/main/java/com/madpoints/webscraper/controller/LoginController.java
@@ -1,14 +1,13 @@
 package com.madpoints.webscraper.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.madpoints.webscraper.entity.User;
+import com.madpoints.webscraper.entity.Login;
 import com.madpoints.webscraper.service.UserService;
 
 @Controller
@@ -24,11 +23,10 @@ public class LoginController {
 	}
 	
 	@PostMapping("/login")
-	public String submit(Model theModel) {
+	public String loginUser(@ModelAttribute("login") Login login, 
+							Model theModel) {
 		
-		List<User> theUsers = userService.getUsers();
-		
-		theModel.addAttribute("user", theUsers);
+		userService.loginUser(login);
 		
 		return "home";
 	}
