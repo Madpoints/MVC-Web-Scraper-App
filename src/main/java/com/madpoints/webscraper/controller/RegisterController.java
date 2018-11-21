9346d7f2037daf8ef4393fb2f28c9f01d5d311d6
John Furtado <jf8846@gmail.com>
2018-11-21 12:16:35 -0500

    user added to regitration page model

--- src/main/java/com/madpoints/webscraper/controller/RegisterController.java
+++ src/main/java/com/madpoints/webscraper/controller/RegisterController.java
@@ -1,17 +1,24 @@
 package com.madpoints.webscraper.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.madpoints.webscraper.entity.User;
 
 @Controller
 @RequestMapping("/register")
 public class RegisterController {
 	
 	@GetMapping("/new")
-	public String showRegistration() {
+	public String showRegistration(Model theModel) {
+		
+		theModel.addAttribute("user", new User());
 		
 		return "register";
 	}
+	
+	// post mapping needed for register and user needs to be added to the model
 
 }
