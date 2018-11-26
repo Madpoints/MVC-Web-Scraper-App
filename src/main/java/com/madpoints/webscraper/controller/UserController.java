8381a794299ec2ff001df0eb2e0c7be1e569c866
John Furtado <jf8846@gmail.com>
2018-11-26 10:03:42 -0500

    get and post mapping for /login, shows login page and logs in user

--- src/main/java/com/madpoints/webscraper/controller/UserController.java
+++ src/main/java/com/madpoints/webscraper/controller/UserController.java
@@ -2,7 +2,12 @@ package com.madpoints.webscraper.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.madpoints.webscraper.entity.Login;
 import com.madpoints.webscraper.service.UserService;
 
 @Controller
@@ -10,5 +15,21 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
+	
+	@GetMapping("/login")
+	public String showLogin(Model theModel) {
+		
+		theModel.addAttribute("login", new Login());
+		
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	public String login(@ModelAttribute("login") Login login, Model theModel) {
+		
+		userService.loginUser(login);
+		
+		return "home";
+	}
 
 }
