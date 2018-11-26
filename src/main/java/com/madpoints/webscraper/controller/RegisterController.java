4179d5557351853657ac8663d968e8f029885fce
John Furtado <jf8846@gmail.com>
2018-11-25 20:04:35 -0500

    register user post mapping

--- src/main/java/com/madpoints/webscraper/controller/RegisterController.java
+++ src/main/java/com/madpoints/webscraper/controller/RegisterController.java
@@ -3,6 +3,8 @@ package com.madpoints.webscraper.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.madpoints.webscraper.entity.User;
@@ -22,5 +24,10 @@ public class RegisterController {
 	}
 	
 	// post mapping needed for register and user needs to be added to the model
-
+	@PostMapping("/registerUser")
+	public String registerUser(@ModelAttribute("user") User user) {
+		
+	
+		return "home";
+	}
 }
