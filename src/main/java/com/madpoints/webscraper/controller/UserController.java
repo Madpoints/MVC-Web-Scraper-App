652e6a13e536e4acba0b556b77c2e4f447f4a265
John Furtado <jf8846@gmail.com>
2018-11-26 10:09:47 -0500

    removed login get mapping

--- src/main/java/com/madpoints/webscraper/controller/UserController.java
+++ src/main/java/com/madpoints/webscraper/controller/UserController.java
@@ -3,7 +3,6 @@ package com.madpoints.webscraper.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,14 +14,6 @@ public class UserController {
 	
 	@Autowired
 	private UserService userService;
-	
-	@GetMapping("/login")
-	public String showLogin(Model theModel) {
-		
-		theModel.addAttribute("login", new Login());
-		
-		return "login";
-	}
 	
 	@PostMapping("/login")
 	public String login(@ModelAttribute("login") Login login, Model theModel) {
