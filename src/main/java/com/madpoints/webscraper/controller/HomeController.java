d6322b5a218b6336337d4982c944410dd292e6bf
John Furtado <jf8846@gmail.com>
2018-11-26 10:10:15 -0500

    display login page

--- src/main/java/com/madpoints/webscraper/controller/HomeController.java
+++ src/main/java/com/madpoints/webscraper/controller/HomeController.java
@@ -1,5 +1,20 @@
 package com.madpoints.webscraper.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.madpoints.webscraper.entity.Login;
+
+@Controller
 public class HomeController {
+	
+	@GetMapping("/login")
+	public String showLogin(Model theModel) {
+		
+		theModel.addAttribute("login", new Login());
+		
+		return "login";
+	}
 
 }
