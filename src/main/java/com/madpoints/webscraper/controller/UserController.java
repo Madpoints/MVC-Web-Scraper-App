56ac4b6b44b2daa1d840bae6229484ec384f7ded
John Furtado <jf8846@gmail.com>
2018-11-27 14:11:13 -0500

    get mapping for delete user

--- src/main/java/com/madpoints/webscraper/controller/UserController.java
+++ src/main/java/com/madpoints/webscraper/controller/UserController.java
@@ -3,8 +3,10 @@ package com.madpoints.webscraper.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.madpoints.webscraper.entity.Login;
@@ -18,7 +20,7 @@ public class UserController {
 	private UserService userService;
 	
 	@PostMapping("/login")
-	public String login(@ModelAttribute("login") Login loginInfo, Model theModel) {
+	public String loginUser(@ModelAttribute("login") Login loginInfo, Model theModel) {
 		
 		userService.loginUser(loginInfo);
 		
@@ -34,5 +36,12 @@ public class UserController {
 	
 		return "redirect:/home/{userId}";
 	}
+	
+	@GetMapping("/delete")
+	public String deleteUser(@RequestParam("userId") int userId) {
+		
+		return "redirect:/login";
+	}
+	
 
 }
