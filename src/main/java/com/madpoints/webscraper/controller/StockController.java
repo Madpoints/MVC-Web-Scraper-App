ab1834df8ce6c20673bcc819b3044a2fb86123e5
John Furtado <jf8846@gmail.com>
2018-12-11 17:05:28 -0500

    /stocks request mapping

--- src/main/java/com/madpoints/webscraper/controller/StockController.java
+++ src/main/java/com/madpoints/webscraper/controller/StockController.java
@@ -1,8 +1,10 @@
 package com.madpoints.webscraper.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/stocks")
 public class StockController {
 	
 	
