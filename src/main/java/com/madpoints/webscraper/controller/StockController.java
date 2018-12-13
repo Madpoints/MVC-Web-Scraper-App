ad1db35aeaa4e15763d7edf4722a53e1bbee40c6
John Furtado <jf8846@gmail.com>
2018-12-13 17:05:22 -0500

    /list get mapping and auto wired stockService

--- src/main/java/com/madpoints/webscraper/controller/StockController.java
+++ src/main/java/com/madpoints/webscraper/controller/StockController.java
@@ -1,12 +1,31 @@
 package com.madpoints.webscraper.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.madpoints.webscraper.entity.Stock;
+import com.madpoints.webscraper.service.StockService;
 
 @Controller
 @RequestMapping("/stocks")
 public class StockController {
 	
+	@Autowired
+	private StockService stockService;
 	
+	@GetMapping("/list")
+	public String getStocks(Model theModel) {
+		
+		List<Stock> stocks = stockService.getStocks();
+		
+		theModel.addAttribute("stocks", stocks);
+		
+		return "stock-list";
+	}
 
 }
