15acc9a9d3a73a7a7672965ad1df58f0bd3398e1
John Furtado <jf8846@gmail.com>
2018-12-13 17:03:28 -0500

    getStocks method returns a list of stocks

--- src/main/java/com/madpoints/webscraper/service/StockService.java
+++ src/main/java/com/madpoints/webscraper/service/StockService.java
@@ -1,5 +1,11 @@
 package com.madpoints.webscraper.service;
 
+import java.util.List;
+
+import com.madpoints.webscraper.entity.Stock;
+
 public interface StockService {
+	
+	public List<Stock> getStocks();
 
 }
