7ca68b45ec1253e35d38ba4ce27fc00bc77d9c5b
John Furtado <jf8846@gmail.com>
2018-12-13 17:04:44 -0500

    getStocks implementation

--- src/main/java/com/madpoints/webscraper/service/StockServiceImpl.java
+++ src/main/java/com/madpoints/webscraper/service/StockServiceImpl.java
@@ -1,5 +1,21 @@
 package com.madpoints.webscraper.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.madpoints.webscraper.dao.StockDAO;
+import com.madpoints.webscraper.entity.Stock;
+
 public class StockServiceImpl implements StockService {
+	
+	@Autowired
+	private StockDAO stockDAO;
+
+	@Override
+	public List<Stock> getStocks() {
+		
+		return stockDAO.getStocks();
+	}
 
 }
