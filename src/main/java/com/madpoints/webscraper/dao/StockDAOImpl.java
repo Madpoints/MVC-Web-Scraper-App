9a1fa66fd8aefaf716560758399573398dedc95a
John Furtado <jf8846@gmail.com>
2018-12-12 19:07:47 -0500

    auto wired session factory

--- src/main/java/com/madpoints/webscraper/dao/StockDAOImpl.java
+++ src/main/java/com/madpoints/webscraper/dao/StockDAOImpl.java
@@ -2,10 +2,16 @@ package com.madpoints.webscraper.dao;
 
 import java.util.List;
 
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.madpoints.webscraper.entity.Stock;
 
 public class StockDAOImpl implements StockDAO {
 	
+	@Autowired
+	private SessionFactory sessionFactory;
+
 	@Override
 	public List<Stock> getStocks() {
 		
