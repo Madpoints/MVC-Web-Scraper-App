eed7c4cd85821fe07ed2f25a97c006cb6999f29b
John Furtado <jf8846@gmail.com>
2018-12-12 19:06:50 -0500

    get stocks interface and implementation

--- src/main/java/com/madpoints/webscraper/dao/StockDAO.java
+++ src/main/java/com/madpoints/webscraper/dao/StockDAO.java
@@ -1,5 +1,11 @@
 package com.madpoints.webscraper.dao;
 
+import java.util.List;
+
+import com.madpoints.webscraper.entity.Stock;
+
 public interface StockDAO {
+	
+	List<Stock> getStocks();
 
 }
--- src/main/java/com/madpoints/webscraper/dao/StockDAOImpl.java
+++ src/main/java/com/madpoints/webscraper/dao/StockDAOImpl.java
@@ -1,5 +1,15 @@
 package com.madpoints.webscraper.dao;
 
+import java.util.List;
+
+import com.madpoints.webscraper.entity.Stock;
+
 public class StockDAOImpl implements StockDAO {
+	
+	@Override
+	public List<Stock> getStocks() {
+		
+		return null;
+	}
 
 }
