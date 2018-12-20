b815d841430b69dda8a5cd407de3c8a22bd814cf
John Furtado <jf8846@gmail.com>
2018-12-20 14:05:44 -0500

    setStocks take in a web driver and returns a list of type stock

--- src/main/java/com/madpoints/webscraper/controller/StockController.java
+++ src/main/java/com/madpoints/webscraper/controller/StockController.java
@@ -1,8 +1,11 @@
 package com.madpoints.webscraper.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -47,9 +50,119 @@ public class StockController {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.finance.yahoo.com/most-active");
 		
+		List<Stock> stocks = setStocks(driver);
+		
 		driver.quit();
 		
-		return null;		
+		return stocks;		
 	}
+	
+	private static List<Stock> setStocks(WebDriver driver) {
+		
+		List<WebElement> stockSymbolElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Symbol']"));
+		List<WebElement> stockNameElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Name']"));
+		List<WebElement> stockPriceElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Price (Intraday)']"));
+		List<WebElement> stockChangeElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Change']"));
+		// escape % char?
+		List<WebElement> stockPercentChangeElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='% Change']"));
+		List<WebElement> stockVolumeElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Volume']"));
+		List<WebElement> stockAvgVolumeElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Avg Vol (3 month)']"));
+		List<WebElement> stockMarketCapElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='Market Cap']"));
+		List<WebElement> stockPeRatioElements = driver.findElements(By.xpath("//table[@data-reactid='73']//td[@aria-label='PE Ratio (TTM)']"));
+
+		List<Stock> stocks = new ArrayList<Stock>();
+		
+		int size = stockSymbolElements.size();
+		int listIndex = 0;
+		
+		for (int index = 0; index < size; index++) {
+			
+			Stock tempStock = new Stock();
+			stocks.add(index, tempStock);
+			
+		}
+		
+		for (WebElement stockSymbolElement : stockSymbolElements) {
+			
+			stocks.get(listIndex).setSymbol(stockSymbolElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockNameElement : stockNameElements) {
+			
+			stocks.get(listIndex).setName(stockNameElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockPriceElement : stockPriceElements) {
+			
+			stocks.get(listIndex).setPrice(stockPriceElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockChangeElement : stockChangeElements) {
+			
+			stocks.get(listIndex).setChange(stockChangeElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockPercentChangeElement : stockPercentChangeElements) {
+			
+			stocks.get(listIndex).setPercentChange(stockPercentChangeElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockVolumeElement : stockVolumeElements) {
+			
+			stocks.get(listIndex).setVolume(stockVolumeElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockAvgVolumeElement : stockAvgVolumeElements) {
+			
+			stocks.get(listIndex).setAvgVolume(stockAvgVolumeElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockMarketCapElement : stockMarketCapElements) {
+			
+			stocks.get(listIndex).setMarketCap(stockMarketCapElement.getText());
+			listIndex++;
+			
+		}
+		
+		listIndex = 0;
+		
+		for (WebElement stockPeRatioElement : stockPeRatioElements) {
+			
+			stocks.get(listIndex).setPeRatio(stockPeRatioElement.getText());
+			listIndex++;
+			
+		}
+		
+		return stocks;
+	}
+
 
 }
