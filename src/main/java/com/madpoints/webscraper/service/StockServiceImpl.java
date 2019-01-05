0300d6a7e031d207e53d9163abd225b3c1161fda
John Furtado <jf8846@gmail.com>
2019-01-05 16:59:20 -0500

    transactional annotation

--- src/main/java/com/madpoints/webscraper/service/StockServiceImpl.java
+++ src/main/java/com/madpoints/webscraper/service/StockServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.madpoints.webscraper.dao.StockDAO;
 import com.madpoints.webscraper.entity.Stock;
@@ -15,18 +16,21 @@ public class StockServiceImpl implements StockService {
 	private StockDAO stockDAO;
 
 	@Override
+	@Transactional
 	public List<Stock> getStocks() {
 		
 		return stockDAO.getStocks();
 	}
 
 	@Override
+	@Transactional
 	public Stock getStock(int stockId) {
 		
 		return stockDAO.getStock(stockId);
 	}
 
 	@Override
+	@Transactional
 	public void saveStock(List<Stock> stocks) {
 		
 		stockDAO.saveStock(stocks);
