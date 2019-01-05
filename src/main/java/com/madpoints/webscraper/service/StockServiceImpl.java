1400a93e3aad10b839ef80e84faa6b435a29e77b
John Furtado <jf8846@gmail.com>
2019-01-04 19:29:45 -0500

    service annotation

--- src/main/java/com/madpoints/webscraper/service/StockServiceImpl.java
+++ src/main/java/com/madpoints/webscraper/service/StockServiceImpl.java
@@ -3,10 +3,12 @@ package com.madpoints.webscraper.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.madpoints.webscraper.dao.StockDAO;
 import com.madpoints.webscraper.entity.Stock;
 
+@Service
 public class StockServiceImpl implements StockService {
 	
 	@Autowired
