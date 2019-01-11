1d997390ffcd884df019ed6441af440c2b56c8a8
John Furtado <jf8846@gmail.com>
2019-01-11 18:12:35 -0500

    saveUserStock takes in a UserStock and saves or updates the db

--- src/main/java/com/madpoints/webscraper/service/UserStockServiceImpl.java
+++ src/main/java/com/madpoints/webscraper/service/UserStockServiceImpl.java
@@ -1,14 +1,33 @@
 package com.madpoints.webscraper.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.madpoints.webscraper.dao.UserStockDAO;
+import com.madpoints.webscraper.entity.Stock;
+import com.madpoints.webscraper.entity.UserStock;
 
 @Service
 public class UserStockServiceImpl implements UserStockService {
 	
 	@Autowired
 	UserStockDAO userStockDAO;
+
+	@Override
+	@Transactional
+	public List<Stock> getUserStocks(int userId) {
+
+		return userStockDAO.getUserStocks(userId);
+	}
+
+	@Override
+	@Transactional
+	public void saveUserStock(UserStock userStock) {
+		
+		userStockDAO.saveUserStock(userStock);
+	}
 
 }
