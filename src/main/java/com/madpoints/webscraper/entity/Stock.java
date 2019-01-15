bad01f1b7e6f1112331256a37543750bceffa08c
John Furtado <jf8846@gmail.com>
2019-01-15 18:55:27 -0500

    one to many annotation for a list of type UserStock for a Stock class, reference for which user owns which stock and the number of shares

--- src/main/java/com/madpoints/webscraper/entity/Stock.java
+++ src/main/java/com/madpoints/webscraper/entity/Stock.java
@@ -1,10 +1,14 @@
 package com.madpoints.webscraper.entity;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -31,6 +35,11 @@ public class Stock {
 //	private String avgVolume;
 //	private String marketCap;
 //	private String peRatio;
+	
+	@OneToMany(mappedBy="user_stock",
+			cascade={CascadeType.PERSIST, CascadeType.MERGE,
+					CascadeType.DETACH, CascadeType.REFRESH})
+	private List<UserStock> userStocks;
 	
 	public Stock() {
 	}
